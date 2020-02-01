3531f950de0d727f729fe771cff9f18b53d05310
j-brn <me@jbrn.eu>
2020-02-01 01:56:09 +0100

    Add score models

--- src/models/game.rs
+++ src/models/game.rs
@@ -19,6 +19,37 @@ impl Player {
     }
 }
 
+pub struct ScoreStack {
+    matches: Vec<Match>
+}
+
+impl ScoreStack {
+    pub fn new() -> Self {
+        Self { matches: Vec::new() }
+    }
+
+    pub fn count(&self) -> u32 {
+        self.matches
+            .iter()
+            .copied()
+            .fold(0, |sum, x| sum + x.points())
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub enum Match {
+    Full,
+    Half
+}
+
+impl Match {
+    pub fn points(self) -> u32 {
+        match self {
+            Match::Full => 3,
+            Match::Half => 1
+        }
+    }
+}
 
 pub struct Desk {
     players: Vec<Player>
